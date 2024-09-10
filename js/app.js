3a93cfe65ef8c18157dd9295713eea4c46908f25
KVoronova <voronova.vs@gmail.com>
2024-09-10 19:44:07 +0300

    create slider with swiper

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 const swiper = new Swiper('.swiper', {
-    
+    direction: 'vertical',
+    speed: 1500,
+    parallax: true,
+    mousewheel: {
+        invert: true,
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      }
 });
